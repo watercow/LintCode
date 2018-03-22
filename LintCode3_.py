a32ec5963ca6108a845bcbf7de134a963bb840bc
watercow <watercow@users.noreply.github.com>
2018-03-22 10:25:45 +0800

    Update LintCode3_.py

--- LintCode3_.py
+++ LintCode3_.py
@@ -1,3 +1,9 @@
-import re
+class Solution:
+    """
+    @param: : An integer
+    @param: : An integer
+    @return: An integer denote the count of digit k in 1..n
+    """
 
-def 
+    def digitCounts(self, k, n):
+        # write your code here
