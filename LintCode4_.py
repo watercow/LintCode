35f137bb4114fb3fe77b95ff5d599505dd03f295
水牛 <watercow@users.noreply.github.com>
2018-03-28 10:44:55 +0800

    Update LintCode4_.py

--- LintCode4_.py
+++ LintCode4_.py
@@ -1,2 +1,8 @@
-class Solution():
+class Solution:
+    """
+    @param n: An integer
+    @return: the nth prime number as description.
+    """
+    def nthUglyNumber(self, n):
+        # write your code here
   
