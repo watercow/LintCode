37056ff021610f2e2d64f50d1f83af6d08e97204
水牛 <watercow@users.noreply.github.com>
2018-04-07 03:32:24 +0000

    Update LintCode5_kthLargestElement.py

--- LintCode5_kthLargestElement.py
+++ LintCode5_kthLargestElement.py
@@ -1,1 +1,9 @@
+class Solution:
+    # @param k & A a integer and an array
+    # @return ans a integer
+    def kthLargestElement(self, k, A):
+        sorted_list = sorted(A,reverse=True)
+        return sorted_list[k-1]
 
+if __name__ == '__main__':
+    print(Solution().kthLargestElement(10,[1,2,3,4,5,6,8,9,10,7]))
