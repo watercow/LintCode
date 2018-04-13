b5b36e9968ca4124cd128cd13c5ccb2afaba6af7
水牛 <watercow@users.noreply.github.com>
2018-04-13 10:25:17 +0800

    Update LintCode6_mergesortedArray.py

--- LintCode6_mergesortedArray.py
+++ LintCode6_mergesortedArray.py
@@ -1,1 +1,28 @@
+class Solution:
+    """
+    @param A: sorted integer array A
+    @param B: sorted integer array B
+    @return: A new sorted integer array
+    """
+    def mergeSortedArray(self, A, B):
+        C = []
+        i = j = 0
+        # 双指针遍历
+        while i < len(A) and j < len(B):
+            if A[i] <= B[j]:
+                C.append(A[i])
+                i += 1
+            else:
+                C.append(B[j])
+                j += 1
+        # 将长数组剩余内容添加
+        if i < len(A):
+            for z in range(i,len(A)):
+                C.append(A[z])
+        if j < len(B):
+            for z in range(j,len(B)):
+                C.append(B[z])
+        return C
 
+if __name__ == '__main__':
+    print(Solution().mergeSortedArray([5,7],[7]))
