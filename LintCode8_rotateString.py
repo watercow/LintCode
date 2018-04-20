f0b78cdba71a8ebb4f89cd165dacc78640d1c633
水牛 <watercow@users.noreply.github.com>
2018-04-20 11:55:45 +0000

    Update LintCode8_rotateString.py

--- LintCode8_rotateString.py
+++ LintCode8_rotateString.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    """
+    @param str: An array of char
+    @param offset: An integer
+    @return: nothing
+    """
+    def rotateString(self, str, offset):
+        # write your code here
