2226e618737e14d6312c23c86606d1707348613e
水牛 <watercow@users.noreply.github.com>
2018-04-23 19:20:00 +0800

    Update LintCode8_rotateString.py

--- LintCode8_rotateString.py
+++ LintCode8_rotateString.py
@@ -6,3 +6,14 @@ class Solution:
     """
     def rotateString(self, str, offset):
         # write your code here
+        len_str = len(str)
+        # 取模用来防止溢出
+        offset = offset % len_str
+        if not offset:
+            new_str = str
+        else:
+            new_str = str[len_str - offset:] + str[0:len_str - offset]
+        return new_str
+
+if __name__ == '__main__':
+    print(Solution().rotateString("abcdefg",8))
