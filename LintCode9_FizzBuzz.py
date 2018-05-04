6750fa3ff13d3c49623e25a7864ace04086b136d
水牛 <watercow@users.noreply.github.com>
2018-05-04 10:52:07 +0800

    Update LintCode9_FizzBuzz.py

--- LintCode9_FizzBuzz.py
+++ LintCode9_FizzBuzz.py
@@ -5,3 +5,17 @@ class Solution:
     """
     def fizzBuzz(self, n):
         # write your code here
+        list = []
+        for i in range(1,n + 1):
+            if not (i % 3) and (i % 5):
+                list.append('fizz')
+            elif not (i % 5) and (i % 3):
+                list.append('buzz')
+            elif not (i % 3) and not (i % 5):
+                list.append('fizz buzz')
+            else:
+                list.append(str(i))
+        return list
+
+if __name__ == '__main__':
+    print(Solution().fizzBuzz(15))
