bbdf2876495be44be56dcc43a457ec8f77ac06ef
水牛 <watercow@users.noreply.github.com>
2018-07-24 23:09:28 +0800

    全连接

--- DeepLearning/tensorflow/两个全连接的神经网络.py
+++ DeepLearning/tensorflow/两个全连接的神经网络.py
@@ -20,6 +20,21 @@ X = tf.placeholder(tf.float32, shape=[None, inputSize])
 y = tf.placeholder(tf.float32, shape=[None, numClasses])
 
 # 参数初始化
+W1 = tf.Variable(tf.truncated_normal([inputSize, numHiddenUnits], stddev = 0.1))
+B1 = tf.Variable(tf.constant(0.1), [numHiddenUnits])
+W2 = tf.Variable(tf.truncated_normal([numHiddenUnits, numClasses], stddev = 0.1))
+B2 = tf.Variable(tf.constant(0.1), [numClasses])
 
+# 网络结构
+hiddenLayerOutput = tf.matmul(X,W1) + B1
+hiddenLayerOutput = tf.nn.relu(hiddenLayerOutput)
+finalOutput = tf.matmul(hiddenLayerOutput, W2) + B2
+finalOutput = tf.nn.relu(finalOutput)
 
+# 网络迭代
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=finalOutput)) # labels真实值 logits预测值
+opt = tf.train.GradientDescentOptimizer(learning_rate=.1).minimize(loss)
+
+correct_prediction = tf.equal(tf.argmax(finalOutput,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
