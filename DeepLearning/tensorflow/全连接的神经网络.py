655802e649648a3386b6b55b54304be8c4a105d0
水牛 <watercow@users.noreply.github.com>
2018-07-29 10:08:41 +0800

    update

--- DeepLearning/tensorflow/全连接的神经网络.py
+++ DeepLearning/tensorflow/全连接的神经网络.py
@@ -51,3 +51,23 @@ for i in range(trainingIterations):
         trainAccuracy = accuracy.eval(session=sess,feed_dict={X:batchInput, y:batchLabels})
         print("step %d, training accuracy %g" % (i, trainAccuracy))
 
+# 之前是单层的神经网络
+# 如果要变成两层的神经网络
+# 第一个隐藏50个神经元 第二个隐层100个神经元，输入输出层不变
+numHiddenUnitsLayer2 = 100
+# 参数初始化应变为
+W1 = tf.Variable(tf.truncated_normal([inputSize, numHiddenUnits], stddev = 0.1))
+B1 = tf.Variable(tf.constant(0.1), [numHiddenUnits])
+W2 = tf.Variable(tf.truncated_normal([numHiddenUnits, numHiddenUnitsLayer2], stddev = 0.1))
+B2 = tf.Variable(tf.constant(0.1), [numHiddenUnitsLayer2])
+W3 = tf.Variable(tf.random_normal([numHiddenUnitsLayer2, numClasses], stddev = 0.1))
+B3 = tf.Variable(tf.constant(0.1), [numClasses])
+
+# 网络结构应变为
+hiddenLayerOutput = tf.matmul(X,W1) + B1
+hiddenLayerOutput = tf.nn.relu(hiddenLayerOutput)
+hiddenLayer2Output = tf.matmul(hiddenLayerOutput, W2) + B2
+hiddenLayer2Output = tf.nn.relu(hiddenLayer2Output)
+finalOutput = tf.matmul(hiddenLayer2Output, W3) + B3
+
+#其余部分无需变化
---
+++ DeepLearning/tensorflow/卷积神经网络.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
