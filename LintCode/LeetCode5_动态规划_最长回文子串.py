a5b34c6cd2b34111f4776696bba7a52f1b7d26d7
水牛 <watercow@users.noreply.github.com>
2018-08-27 10:11:56 +0800

    Update LeetCode5_动态规划_最长回文子串.py

--- LintCode/LeetCode5_动态规划_最长回文子串.py
+++ LintCode/LeetCode5_动态规划_最长回文子串.py
@@ -1,1 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        '''
+        1.动态规划 O(n^2)
+        '''
 
+
+        '''
+        2.中心扩展 O(n^2)
+        '''
+
+
+        '''
+        3.Manacher算法 O(n)
+        '''
+        
