e15eff4a9629bfbe0722ba1b1a066b4120a98b08
水牛 <watercow@users.noreply.github.com>
2018-08-28 22:22:03 +0800

    update

---
+++ DeepLearning/tensorflow/CNN/猫狗识别/train.py
@@ -0,0 +1,28 @@
+import dataset
+import tensorflow as tf
+import time
+from datetime import timedelta
+import math
+import random
+import numpy as np
+
+# 安装openCV 打开anaconda3的命令行，
+# 运行命令conda install --channel https://conda.anaconda.org/menpo opencv3
+
+# Adding Seed so that random initialization is consistent
+from numpy.random import seed
+seed(10)
+from tensorflow import set_random_seed
+set_random_seed(20)
+
+batch_size = 32 #一次迭代32张图像，训练集一共1000张（猫500/狗500）
+
+# Prepare input data
+classes = ['dogs', 'cats']
+num_classes = len(classes)
+
+# 20% of the data will automatically be used for validation
+validation_size = 0.2
+img_size = 64 # 图片有大有小,但全连接大小固定，所以要求输入图像大小一致，设置为h=w=64
+num_channels = 3
+train_path = 'training_data'
