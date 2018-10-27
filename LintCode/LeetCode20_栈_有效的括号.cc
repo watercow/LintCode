ea7df0018b80fa3b7521b6d9a2057730b7d39543
水牛 <watercow@users.noreply.github.com>
2018-10-27 15:48:23 +0800

    Update LeetCode20_栈_有效的括号.cc

--- LintCode/LeetCode20_栈_有效的括号.cc
+++ LintCode/LeetCode20_栈_有效的括号.cc
@@ -1,1 +1,36 @@
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <stack>
 
+using namespace std;
+
+class Solution {
+public:
+	bool isValid(string s) {
+		stack<char> st;
+		for (char &c : s) {
+			if (c == '(' || c == '[' || c == '{')
+				st.push(c);
+			if (c == ')')
+				if (st.empty() || st.top() != '(')
+					return false;
+				else st.pop();
+			if (c == ']')
+				if (st.empty() || st.top() != '[')
+					return false;
+				else st.pop();
+			if (c == '}')
+				if (st.empty() || st.top() != '{')
+					return false;
+				else st.pop();
+		}
+		return st.empty();
+	}
+};
+
+int main()
+{
+	cout << Solution().isValid("[[])");
+    return 0;
+}
