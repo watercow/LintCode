c971798726d89cb373d7666c1c5852499283a76b
水牛 <watercow@users.noreply.github.com>
2018-12-01 13:16:05 +0800

    Create LeetCode59_螺旋矩阵II.cc

---
+++ LintCode/LeetCode59_螺旋矩阵II.cc
@@ -0,0 +1,32 @@
+class Solution {
+public:
+	vector<vector<int>> generateMatrix(int n) {
+		vector<vector<int>> res(n, vector<int>(n, 0));
+		int left = 0, right = n - 1, top = 0, bottom = n - 1;
+		int num = 1;
+
+		while (true) {
+			for (int i = left; i <= right; i++) {
+				res[top][i] = num++;
+			}
+			if (++top > bottom) break;
+
+			for (int i = top; i <= bottom; i++) {
+				res[i][right] = num++;
+			}
+			if (--right < left) break;
+
+			for (int i = right; i >= left; i--) {
+				res[bottom][i] = num++;
+			}
+			if (--bottom < top) break;
+
+			for (int i = bottom; i >= top; i--) {
+				res[i][left] = num++;
+			}
+			if (++left > right) break;
+		}
+
+		return res;
+	}
+};
